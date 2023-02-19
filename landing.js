7d6d57d4e1715da29912bf7d732b54f7778e2ef4
Akshat Jain <84455686+AK-shat-JAIN@users.noreply.github.com>
2023-02-20 02:23:39 +0530

    landing page redirects to index page automatically after 3.5 seconds

--- landing.js
+++ landing.js
@@ -1,9 +1,9 @@
-const logo = document.querySelectorAll("#logo path");
-for(let i=0;i<logo.length;i++){
-    console.log(`letter ${i} is ${logo[i].getTotalLength()}`);
-}
-
-// function redirectpage(){
-//     window.location="index.html";
+// const logo = document.querySelectorAll("#logo path");
+// for(let i=0;i<logo.length;i++){
+//     console.log(`letter ${i} is ${logo[i].getTotalLength()}`);
 // }
-// setTimeout('redirectpage()', 3500);
+
+function redirectpage(){
+    window.location="index.html";
+}
+setTimeout('redirectpage()', 3500);
