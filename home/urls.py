6cfbff4b476bfd1769cc428987269b584e61af1c
Vivek Prajapati <62784060+codervivek5@users.noreply.github.com>
2023-03-02 11:47:33 +0530

    dublicate file

--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('about', views.about, name='about'),
-   
-]
