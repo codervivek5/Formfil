85de6ec9cc3ee4e7d4cc63b9a72d92501a78238f
Harshit <2000520310026@ietlucknow.ac.in>
2023-03-15 19:29:17 +0530

    Revert "Now user can login using email/password and gets redirected to userdashboard page. User cannot access dashboard page if is not logged in"
    This reverts commit 6999d756e8dc89c7ff7154447c85a1aae95e8103.

--- Django-FormFil/FormFil/home/urls.py
+++ Django-FormFil/FormFil/home/urls.py
@@ -5,12 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('login/', views.loginpage, name='loginpage'),
-    path('loginemail/', views.loginemail, name='loginemail'),
     path('signup/', views.signup, name='signup'),
     path('about/', views.about, name='about'),
     path('cart/', views.cart, name='cart'),
     path('contact/', views.contactus, name='contactus'),
     path('service/', views.service, name='service'),
-    path('userdashboard/', views.userdashboard, name='userdashboard'),
-    path('logout1/', views.logout1, name='logout1'),
 ]
